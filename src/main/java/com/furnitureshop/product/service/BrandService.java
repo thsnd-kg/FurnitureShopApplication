abb21814fc4fb5a522178e364d920f992810d2d5
Phạm Tấn khang <phamtankhang2001@gmail.com>
2022-04-05 21:11:22 +0700

    Add API for category and brand

---
+++ src/main/java/com/furnitureshop/product/service/BrandService.java
@@ -0,0 +1,15 @@
+package com.furnitureshop.product.service;
+
+import com.furnitureshop.product.dto.BrandDto;
+import com.furnitureshop.product.entity.Brand;
+
+import java.util.List;
+
+public interface BrandService {
+    List<Brand> getBrands();
+    List<Brand> getBrandsActive();
+    Brand getBrandById(Long brandId);
+    Brand createBrand(BrandDto dto);
+    Brand updateBrand(BrandDto dto);
+    Boolean deleteBrand(Long brandId);
+}
