4df0718ebf77a30bebc6bb7538e1faf2d4c19cb7
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-04-25 20:50:04 +0700

    abcxyz

--- src/main/java/com/furnitureshop/product/controller/OptionController.java
+++ src/main/java/com/furnitureshop/product/controller/OptionController.java
@@ -3,10 +3,12 @@ package com.furnitureshop.product.controller;
 import com.furnitureshop.common.ResponseHandler;
 import com.furnitureshop.product.service.OptionService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/option")
 public class OptionController {
