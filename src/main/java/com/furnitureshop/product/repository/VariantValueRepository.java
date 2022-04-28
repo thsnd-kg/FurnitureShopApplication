910badaf528bf9d92d6d4d6bae7413797a264ad2
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-04-28 17:53:03 +0700

    .

--- src/main/java/com/furnitureshop/product/repository/VariantValueRepository.java
+++ src/main/java/com/furnitureshop/product/repository/VariantValueRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VariantValueRepository extends JpaRepository<VariantValue, VariantValuePK> {
-    @Query("SELECT DISTINCT a.optionValue FROM VariantValue a WHERE a.productVariant.product.productId = ?1")
+    @Query("SELECT DISTINCT vv.optionValue, vv.optionImage FROM VariantValue vv WHERE vv.productVariant IN (SELECT pv.variantId FROM ProductVariant pv WHERE pv.product = ?1)")
     VariantValue getOptionValue(Long productId);
 }
