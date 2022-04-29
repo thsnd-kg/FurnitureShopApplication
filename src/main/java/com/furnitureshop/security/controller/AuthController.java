a9985218307cecfd283d8eae6b301769d17f2988
Bui Minh Huy <buiminhhuyqnam@gmail.com>
2022-04-30 01:06:09 +0700

    Update AuthController.java

--- src/main/java/com/furnitureshop/security/controller/AuthController.java
+++ src/main/java/com/furnitureshop/security/controller/AuthController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
