4a5f551f9c37e57a927ce3e72db18d76ec00eb6c
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-05-01 07:15:54 +0700

    .

--- src/main/java/com/furnitureshop/product/dto/category/GetCategoryDto.java
+++ src/main/java/com/furnitureshop/product/dto/category/GetCategoryDto.java
@@ -11,12 +11,14 @@ public class GetCategoryDto {
     private Long categoryId;
     private String categoryName;
     private String categoryDesc;
+    private GetCategoryDto parent;
     private List<GetOptionDto> options;
 
     public GetCategoryDto(Category category) {
         this.categoryId = category.getCategoryId();
         this.categoryName = category.getCategoryName();
         this.categoryDesc = category.getCategoryDesc();
+        this.parent = new GetCategoryDto(category.getParent());
         this.options = category.getOptions().stream().map(GetOptionDto::new).collect(Collectors.toList());
     }
 }
