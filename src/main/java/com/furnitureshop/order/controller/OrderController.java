8a776fd8596ff1b7de61a05c86968a39934ca732
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-05-04 09:57:11 +0700

    Merge branch 'product'

--- src/main/java/com/furnitureshop/order/controller/OrderController.java
+++ src/main/java/com/furnitureshop/order/controller/OrderController.java
@@ -1,9 +1,13 @@
 package com.furnitureshop.order.controller;
 
+import com.furnitureshop.common.ResponseHandler;
 import com.furnitureshop.order.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @CrossOrigin
@@ -15,5 +19,15 @@ public class OrderController {
     @Autowired
     public OrderController(OrderService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public Object getOrders() {
+        return ResponseHandler.getResponse(service.getOrders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{order-id}")
+    public Object getOrder(@PathVariable("order-id") Long orderId) {
+        return ResponseHandler.getResponse(service.getOrder(orderId), HttpStatus.OK);
     }
 }
