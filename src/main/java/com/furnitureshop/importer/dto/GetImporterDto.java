2879feab32b9c032e8174eb625a0b7609cede6ef
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-05-19 17:06:08 +0700

    .

--- src/main/java/com/furnitureshop/importer/dto/GetImporterDto.java
+++ src/main/java/com/furnitureshop/importer/dto/GetImporterDto.java
@@ -14,7 +14,7 @@ public class GetImporterDto {
     private final Long importId;
     private final String importDesc;
     private final Integer totalPrice;
-    private final User importer;
+    private final String emailImporter;
     private final LocalDate createdAt;
     private final List<GetImporterDetailDto> importDetails;
 
@@ -22,7 +22,7 @@ public class GetImporterDto {
         this.importId = importer.getImportId();
         this.importDesc = importer.getImportDesc();
         this.totalPrice = importer.getTotalPrice();
-        this.importer = importer.getUser();
+        this.emailImporter = importer.getUser().getEmail();
         this.createdAt = importer.getCreatedAt().toLocalDate();
         this.importDetails = importer.getImportDetails().isEmpty()
                 ? new ArrayList<>()
