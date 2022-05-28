74479c4c04eac7a634e9c1af68c6572385404f24
Ngô Dương Kha <ngoduongkhakg2001@gmail.com>
2022-05-28 20:35:55 +0700

    .

--- src/main/java/com/furnitureshop/product/dto/category/GetCategoryDto.java
+++ src/main/java/com/furnitureshop/product/dto/category/GetCategoryDto.java
@@ -16,7 +16,6 @@ public class GetCategoryDto {
     private final Boolean isDeleted;
     private final Long parentId;
     private final List<GetOptionDto> options;
-    private final List<GetProductDto> products;
 
     public GetCategoryDto(Category category) {
         this.categoryId = category.getCategoryId();
@@ -24,7 +23,6 @@ public class GetCategoryDto {
         this.categoryDesc = category.getCategoryDesc();
         this.isDeleted = category.getIsDeleted();
         this.options = category.getOptions().isEmpty() ? new ArrayList<>() : category.getOptions().stream().map(GetOptionDto::new).collect(Collectors.toList());
-        this.products = category.getProducts().isEmpty() ? new ArrayList<>() : category.getProducts().stream().map(GetProductDto::new).collect(Collectors.toList());
         this.parentId = category.getParent() != null ? category.getParent().getCategoryId() : null;
     }
 }
